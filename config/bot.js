2cc7b9d8ac0ddb850baed56d9a7725d50568502d
Reiven <discord10292002@gmail.com>
2022-04-13 21:39:07 +0100

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     discord: {
-        token: 'OTYyNzQxOTk2MTE0NTUwODM0.YlL9Wg.bxs8dL2RdXvlX3x2KP0a2Z9MTm0',
+        token: process.env.token,
         prefix: '!',
     },
 };
