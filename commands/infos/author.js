189c1082a816f490444fff8e7068fcc8b51c5e84
Reiven <discord10292002@gmail.com>
2022-04-14 00:48:09 +0100

    bye

--- commands/infos/author.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'author',
-    aliases: [],
-    category: 'Infos',
-    utilisation: '{prefix}author',
-
-    execute(client, message) {
-    }
-};
--- commands/infos/uptime.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'uptime',
-    aliases: [],
-    category: 'Infos',
-    utilisation: '{prefix}uptime',
-
-    execute(client, message) {
-    }
-};
