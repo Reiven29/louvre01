dc1aee776c2baf55f3e0817d8f6efd75f1c75083
Reiven <discord10292002@gmail.com>
2022-04-13 02:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const client = new Discord.Client();
 
 client.config = require('./config/bot');
 
-const CONFIG = require("./config/bot");
-
 client.on('ready', () => {
     console.log('Ready!');
-    CONFIG(client);
 });
 
 client.login(client.config.discord.token);
