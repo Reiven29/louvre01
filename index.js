e0988aba40a75b4a25827c54db0683c79f549cea
Reiven <discord10292002@gmail.com>
2022-04-14 01:42:30 +0100

    nice one

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const bot = new Discord.Client({ disableEveryone: true });
 
 const Profile = require("./profile.js");
 
+const express = require('express')
+const canvas = require('canvas')
+const PORT = process.env.PORT || 5000
+
+express()
+    .get('/', (req, res) => res.send({ version: canvas.version, cairoVersion: canvas.cairoVersion }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
 client.login(process.env.token);
